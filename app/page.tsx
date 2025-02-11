80719917f8a68e2cb2c485b16d7763e5c6a3e8a7
Vineet Katiyar <vineetkatiyar98@gmail.com>
2025-02-11 07:57:45 +0530

    changes in home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Search } from "@/components/search";
 export default async function HomePage() {
   return (
     <div className="w-full h-screen">
+      <h1 className="text-center text-3xl">Home Page</h1>
       <Search />
     </div>
   );
